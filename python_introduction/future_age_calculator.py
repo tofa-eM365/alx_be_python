fb3d3db5fdb0e3d63d006cd5d016a40de783e1ca
Emmanuel Tofa <emmauel.t@m365connect.com>
2024-07-19 12:35:02 +0200

    calculate future age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+# Prompting the user to input their current age
+current_age = int(input("How old are you? "))
+
+# Calculating the user's age in the year 2050
+future_year = 2050
+current_year = 2024
+years_to_add = future_year - current_year
+future_age = current_age + years_to_add
+
+# Printing the user's age in 2050
+print(f"In 2050, you will be {future_age} years old.")
