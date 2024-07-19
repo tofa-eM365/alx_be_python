6773c38ac6feb9e0ee9a6583e1c50a015a57efec
Emmanuel Tofa <emmauel.t@m365connect.com>
2024-07-19 12:26:48 +0200

    simple interest programme

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Defining the principal amount, rate of interest, and time period
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculating the simple interest
+interest = principal * rate * time
+
+# Printing the calculated interest
+print(f"The simple interest is: {interest}")
